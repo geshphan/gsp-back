3785c338f61c397408603412f5debfe40a93dbfe
wj.lee <wj.lee@fusionsoft.co.kr>
2023-08-22 11:56:44 +0900

    :wrench: Add HttpRedirectConfig
    Added configuration file to redirect http requests to https request

---
+++ src/main/java/yemenshi/gsp/todo_list/config/HttpRedirectConfig.java
@@ -0,0 +1,42 @@
+package yemenshi.gsp.todo_list.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HttpRedirectConfig {
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+
+        };
+
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(80);
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+
+        return connector;
+    }
+}
