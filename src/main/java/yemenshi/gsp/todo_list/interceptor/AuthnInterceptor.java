5f1f868b8b140f198bdf2efa4ac286021e89abd3
ujoonnee <dnwns0933@naver.com>
2023-09-11 18:03:17 +0900

    :triangular_flag_on_post: Set session timeout
    Session of a user would be expired within certain amount of timeout limit

--- src/main/java/yemenshi/gsp/todo_list/interceptor/AuthnInterceptor.java
+++ src/main/java/yemenshi/gsp/todo_list/interceptor/AuthnInterceptor.java
@@ -7,25 +7,63 @@ import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Slf4j
 public class AuthnInterceptor implements HandlerInterceptor {
+
+    private static final int SESSION_TIME_LIMIT = 300;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
 
-        boolean isLoggedIn = session.getAttribute("user") != null;
-        // FIXME: If session exists, compare between user information in session and request body.
-
-        if (!isLoggedIn) {
-            ObjectMapper om = new ObjectMapper();
-            response.setContentType("application/json");
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            response.getWriter().write(om.writeValueAsString("Not logged in or session has been invalidated."));
-
+        if (isLoggedIn(response, session)) {
             return false;
         }
 
+        if (isSessionExpired(response, session)) {
+            return false;
+        }
+
+        refreshLastAccessTime(session);
+
         return true;
+    }
+
+    private static void refreshLastAccessTime(HttpSession session) {
+        session.setAttribute("accessTimeMillis", LocalDateTime.now());
+    }
+
+    private static boolean isSessionExpired(HttpServletResponse response, HttpSession session) throws IOException {
+        LocalDateTime lastAccessTime = (LocalDateTime) session.getAttribute("accessTimeMillis");
+        long duration = Duration.between(lastAccessTime, LocalDateTime.now()).getSeconds();
+        boolean isExpiredSession = duration >= SESSION_TIME_LIMIT;
+        if (isExpiredSession) {
+            session.invalidate();
+            setResponseForDeniedAccess(response);
+
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isLoggedIn(HttpServletResponse response, HttpSession session) throws IOException {
+        if (session.getAttribute("user") == null) {
+            setResponseForDeniedAccess(response);
+
+            return true;
+        }
+        return false;
+    }
+
+    private static void setResponseForDeniedAccess(HttpServletResponse response) throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(om.writeValueAsString("Not logged in or session has been invalidated."));
     }
 }
